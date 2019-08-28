a31b1727bf73bd25455fe64bc6d2b179cb0986f4
Ciprian Dorin Craciun <ciprian@volution.ro>
2019-08-28 18:02:07 +0300

    [feature]  Add initial TCP frontend/backend related declares

--- sources/default_declares.py
+++ sources/default_declares.py
@@ -21,6 +21,7 @@ def declare_defaults (_configuration) :
 	declare_defaults_timeouts (_configuration)
 	declare_defaults_servers (_configuration)
 	declare_defaults_http (_configuration)
+	declare_defaults_tcp (_configuration)
 	declare_defaults_logging (_configuration)
 	declare_defaults_stats (_configuration)
 	declare_defaults_error_pages (_configuration)
@@ -34,6 +35,13 @@ def declare_http_frontend (_configuration) :
 	declare_http_frontend_stats (_configuration)
 	declare_http_frontend_logging (_configuration)
 	declare_http_frontend_stick (_configuration)
+
+
+def declare_tcp_frontend (_configuration) :
+	declare_tcp_frontend_connections (_configuration)
+	declare_tcp_frontend_timeouts (_configuration)
+	declare_tcp_frontend_logging (_configuration)
+	declare_tcp_frontend_stick (_configuration)
 
 
 def declare_http_backend (_configuration) :
@@ -424,6 +432,68 @@ def declare_http_backend_server_timeouts (_configuration) :
 	])
 
 
+
+
+def declare_defaults_tcp (_configuration) :
+	_configuration.declare_group (
+			"TCP",
+			# FIXME: ...
+	)
+
+
+def declare_tcp_frontend_connections (_configuration) :
+	_configuration.declare_group (
+			"Connections",
+			statement_choose_if ("$?frontend_enabled", "enabled", "disabled"),
+			("maxconn", "$+frontend_max_connections_active_count"),
+			("backlog", "$+frontend_max_connections_backlog_count"),
+			("mode", "tcp"),
+			order = 2000 + 100,
+	)
+
+def declare_tcp_frontend_timeouts (_configuration) :
+	_configuration.declare_group (
+			"Timeouts",
+			order = 2000 + 101,
+	)
+
+def declare_tcp_frontend_logging (_configuration) :
+	_configuration.declare_group (
+			"Logging",
+			("option", "tcplog"),
+			("log-format", "$\"logging_tcp_format"),
+			order = 7000 + 400,
+	)
+
+def declare_tcp_frontend_stick (_configuration) :
+	# FIXME:  Make this configurable!
+	_configuration.declare_group (
+			"Stick tables",
+			("stick-table",
+					"type", "ip",
+					"size", 1024 * 1024,
+					"expire", "3600s",
+					"store", ",".join ((
+							"conn_cur",
+							"conn_cnt",
+							"conn_rate(60s)",
+							"sess_cnt",
+							"sess_rate(60s)",
+							"bytes_in_cnt",
+							"bytes_in_rate(60s)",
+							"bytes_out_cnt",
+							"bytes_out_rate(60s)",
+						))
+			),
+			statement_choose_if ("$?frontend_tcp_stick_track",
+				("tcp-request", "connection", "track-sc0",
+						statement_choose_match ("$frontend_tcp_stick_source",
+								("src", "src"),
+						)
+				),
+			),
+			order = 5000 + 290,
+		)
 
 
 def declare_tcp_backend_connections (_configuration) :
