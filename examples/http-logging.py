c556e9c1771e4a2af086914470f23838b9fd12dd
Ciprian Dorin Craciun <ciprian@volution.ro>
2019-12-16 18:05:07 +0200

    [examples]  Add HTTP logging example

--- examples/http-logging.py
+++ examples/http-logging.py
@@ -5,6 +5,7 @@ import ha
 
 _ha = ha.haproxy (
 		minimal_configure = True,
+		frontend_logging_configure = True,
 	)
 
 
@@ -12,8 +13,8 @@ _fe = _ha.http_frontends.basic ()
 _be = _ha.http_backends.basic (frontend = _fe)
 
 
-_fe.requests.redirect_domain_with_www ("site-with-www.example.com")
-_fe.requests.redirect_domain_without_www ("site-without-www.example.com")
+_fe.requests.capture_logging ()
+_fe.responses.capture_logging ()
 
 
 _ha.output_stdout ()
