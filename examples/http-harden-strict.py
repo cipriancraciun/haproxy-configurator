b4d70a8413fa96688569017e0853a02f0072bd37
Ciprian Dorin Craciun <ciprian@volution.ro>
2019-12-16 19:27:18 +0200

    [examples]  Add hardening examples

---
+++ examples/http-harden-strict.py
@@ -0,0 +1,23 @@
+
+
+import ha
+
+
+_ha = ha.haproxy (
+		minimal_configure = True,
+		http_harden_level = "strict",
+	)
+
+
+_fe = _ha.http_frontends.basic ()
+_be = _ha.http_backends.basic (frontend = _fe)
+
+
+_fe.requests.harden_enable ()
+
+_fe.requests.harden_all ()
+_fe.responses.harden_all ()
+
+
+_ha.output_stdout ()
+
