ae706f00bf1ea766cc37e38c73146a73178099a5
Ciprian Dorin Craciun <ciprian@volution.ro>
2019-12-16 23:38:13 +0200

    [examples]  Add basic HTTP authentication example

---
+++ examples/http-authenticate-basic.py
@@ -0,0 +1,30 @@
+
+
+import ha
+
+
+_ha = ha.haproxy (
+		minimal_configure = True,
+	)
+
+
+_fe = _ha.http_frontends.basic ()
+_be = _ha.http_backends.basic (frontend = _fe)
+
+
+_operators = _ha.credentials_create ("operators", "example.com")
+_operators.declare_user ("operator", "zeregigojacuyixu")
+
+
+
+
+_fe.requests.authenticate (
+		_operators,
+		_acl = _fe.acls.host ("private.example.com"),
+	)
+
+
+
+
+_ha.output_stdout ()
+
